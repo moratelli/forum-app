f56093ebb2e70c0694b61d44f39971b3ded18f15
Pedro Moratelli <pedromoratelli@gmail.com>
2021-12-03 13:22:10 -0300

    feat: setup typeorm, add entities

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import Redis from "ioredis";
+import { createConnection } from "typeorm";
 
 console.log(process.env.NODE_ENV);
 require("dotenv").config();
@@ -13,49 +14,56 @@ declare module "express-session" {
   }
 }
 
-const app = express();
-const router = express.Router();
-const redis = new Redis({
-  port: Number(process.env.REDIS_PORT),
-  host: process.env.REDIS_HOST,
-  password: process.env.REDIS_PASSWORD,
-});
-const RedisStore = connectRedis(session);
-const redisStore = new RedisStore({
-  client: redis,
-});
+const main = async () => {
+  const app = express();
+  const router = express.Router();
 
-app.use(
-  session({
-    store: redisStore,
-    name: process.env.COOKIE_NAME,
-    secret: String(process.env.SESSION_SECRET),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      path: "/",
-      httpOnly: true,
-      secure: false,
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-  } as any)
-);
+  await createConnection();
 
-app.use(router);
-router.get("/", (req, res, next) => {
-  if (!req.session!.userid) {
-    req.session!.userid = Number(req.query.userid);
-    console.log("UserID is set!");
-    req.session!.loadedCount = 0;
-  } else {
-    req.session!.loadedCount = Number(req.session!.loadedCount) + 1;
-  }
+  const redis = new Redis({
+    port: Number(process.env.REDIS_PORT),
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD,
+  });
+  const RedisStore = connectRedis(session);
+  const redisStore = new RedisStore({
+    client: redis,
+  });
 
-  res.send(
-    `userid: ${req.session!.userid}, loadedCount: ${req.session!.loadedCount}`
+  app.use(
+    session({
+      store: redisStore,
+      name: process.env.COOKIE_NAME,
+      secret: String(process.env.SESSION_SECRET),
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        path: "/",
+        httpOnly: true,
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24,
+      },
+    } as any)
   );
-});
 
-app.listen({ port: process.env.SERVER_PORT }, () => {
-  console.log(`Server ready on port ${process.env.SERVER_PORT}`);
-});
+  app.use(router);
+  router.get("/", (req, res, next) => {
+    if (!req.session!.userid) {
+      req.session!.userid = Number(req.query.userid);
+      console.log("UserID is set!");
+      req.session!.loadedCount = 0;
+    } else {
+      req.session!.loadedCount = Number(req.session!.loadedCount) + 1;
+    }
+
+    res.send(
+      `userid: ${req.session!.userid}, loadedCount: ${req.session!.loadedCount}`
+    );
+  });
+
+  app.listen({ port: process.env.SERVER_PORT }, () => {
+    console.log(`Server ready on port ${process.env.SERVER_PORT}`);
+  });
+};
+
+main();
